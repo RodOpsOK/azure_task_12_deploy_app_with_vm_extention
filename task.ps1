6ed36cd94e3b4c69035d16417cc98575c3caac67
Oleh K <hmkl.public@gmail.com>
2026-08-14 17:18:02 +0200

    Edited brunch

--- task.ps1
+++ task.ps1
@@ -49,7 +49,7 @@ $Params = @{
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
     ProtectedSettings  = @{
-        fileUris = @('https://raw.githubusercontent.com/RodOpsOK/azure_task_12_deploy_app_with_vm_extention/refs/heads/develop/install-app.sh')
+        fileUris = @('https://raw.githubusercontent.com/RodOpsOK/azure_task_12_deploy_app_with_vm_extention/refs/heads/main/install-app.sh')
         commandToExecute = './install-app.sh'
     }
 }
